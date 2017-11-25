d880f3f2c08f235526763c0769d3f253e670c736
Abdul Dakkak <abduld@wolfram.com>
2017-11-25 13:51:52 -0600

    fix typos

--- tensorflow-agent/main.go
+++ tensorflow-agent/main.go
@@ -11,9 +11,7 @@ import (
 )
 
 func main() {
-	tensorflow.Register()
-
-	rootCmd, err := cmd.NewRootCommand(tensorflow.FrameworkManifest)
+	rootCmd, err := cmd.NewRootCommand(tensorflow.Register, tensorflow.FrameworkManifest)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
