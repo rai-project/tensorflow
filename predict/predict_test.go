0555bf5aa940600d8c389b22678bafca50e7b347
Cheng Li <cli99@illinois.edu>
2018-03-10 16:49:18 -0600

    update test

--- predict/predict_test.go
+++ predict/predict_test.go
@@ -2,14 +2,24 @@ package predict
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
+	sourcepath "github.com/GeertJohan/go-sourcepath"
+	"github.com/anthonynsimon/bild/imgio"
+	"github.com/anthonynsimon/bild/transform"
 	"github.com/k0kubun/pp"
 	"github.com/rai-project/config"
-	"github.com/rai-project/dlframework"
+	"github.com/rai-project/dlframework/framework/options"
+	"github.com/rai-project/go-mxnet-predictor/utils"
 	tf "github.com/rai-project/tensorflow"
 	"github.com/stretchr/testify/assert"
 	context "golang.org/x/net/context"
+)
+
+var (
+	graph_url    = "https://s3.amazonaws.com/store.carml.org/models/tensorflow/models/bvlc_alexnet_1.0/frozen_model.pb"
+	features_url = "http://data.dmlc.ml/mxnet/models/imagenet/synset.txt"
 )
 
 func XXXTestPredictLoad(t *testing.T) {
@@ -27,8 +37,8 @@ func XXXTestPredictLoad(t *testing.T) {
 	imgPredictor, ok := predictor.(*ImagePredictor)
 	assert.True(t, ok)
 
-	assert.NotEmpty(t, imgPredictor.imageDimensions)
-	assert.NotEmpty(t, imgPredictor.meanImage)
+	assert.NotEmpty(t, imgPredictor.tfGraph)
+	assert.NotEmpty(t, imgPredictor.tfSession)
 
 }
 
@@ -38,17 +48,27 @@ func TestPredictInference(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotEmpty(t, model)
 
-	opts := dlframework.PredictionOptions{}
-	predictor, err := New(*model, opts)
+	ctx := context.Background()
+	opts := options.New(options.Context(ctx))
+
+	predictor, err := New(*model, options.WithOptions(opts))
 	assert.NoError(t, err)
 	assert.NotEmpty(t, predictor)
 	defer predictor.Close()
 
-	ctx := context.Background()
-	err = predictor.Load(ctx, model, opts)
-	assert.NoError(t, err)
+	// load test image for predction
+	img, err := imgio.Open(filepath.Join(sourcepath.MustAbsoluteDir(), "..", "scratch/data", "beautiful-running-horse.jpg"))
+	if err != nil {
+		panic(err)
+	}
+	// preprocess
+	resized := transform.Resize(img, 224, 224, transform.Linear)
+	res, err := utils.CvtImageTo2DArray(resized)
+	if err != nil {
+		panic(err)
+	}
 
-	preds, err := predictor.Predict(ctx, "http://buzzsharer.com/wp-content/uploads/2015/06/beautiful-running-horse.jpg")
+	preds, err := predictor.Predict(ctx, res)
 	assert.NoError(t, err)
 	if err != nil {
 		return
@@ -58,9 +78,7 @@ func TestPredictInference(t *testing.T) {
 		pp.Println(preds)
 	}
 
-	preds.Sort()
-
-	pp.Println(preds.Take(3))
+	pp.Println(preds[0])
 
 }
 
