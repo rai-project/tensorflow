f405659c4df137cd77abd90a3d3d0c299f932165
Cheng Li <cli99@illinois.edu>
2019-03-09 15:17:22 -0600

    work

--- predictor/utils.go
+++ predictor/utils.go
@@ -47,9 +47,9 @@ func decodeJpegGraph() (graph *tf.Graph, input, output tf.Output, err error) {
 	return graph, input, output, err
 }
 
-func makeTensorFromBytes(b []byte) (*tf.Tensor, error) {
+func makeTensorFromBytes(b interface{}) (*tf.Tensor, error) {
 	// DecodeJpeg uses a scalar String-valued tensor as input.
-	tensor, err := tf.NewTensor(string(b))
+	tensor, err := tf.NewTensor(b)
 	if err != nil {
 		return nil, err
 	}
