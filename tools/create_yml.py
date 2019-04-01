574163a835532f6a2c17cb37780dd6dfb084243a
jtang10 <tangjingning@gmail.com>
2019-04-01 14:37:31 -0500

    adding detection model in progress
    added several yml. Uploaded all the coco models to s3. creating new scripts for easy download, creating repetitive yml, etc

---
+++ tools/create_yml.py
@@ -0,0 +1,11 @@
+import os
+
+def create_yml(model_name):
+    yml_dir = os.path.join(os.getcwd(), "../builtin_models")
+    sample_yml = "SSD_MobileNet_v2_COCO.yml"
+    target_yml = model_name + ".yml"
+    with open()
+    print(yml_dir)
+
+if __name__ == "__main__":
+    create_yml("sample")
