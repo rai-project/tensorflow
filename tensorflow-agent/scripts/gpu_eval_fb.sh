f8628e0237caf20a354461eb52d0e4eba8c7ba21
Frank Wei <w.wei@alibaba-inc.com>
2019-07-23 15:55:21 -0700

    add env GPU_DEVICE_ID=0

--- tensorflow-agent/scripts/gpu_eval_fb.sh
+++ tensorflow-agent/scripts/gpu_eval_fb.sh
@@ -7,6 +7,7 @@ NUMPREDS=5
 DUPLICATE_INPUT=$(($NUMPREDS * $BATCHSIZE))
 OUTPUTFOLDER=output
 DATABASE_NAME=carml
+GPU_DEVICE_ID=0
 
 cd ..
 
@@ -20,23 +21,24 @@ export CARML_TF_DISABLE_OPTIMIZATION=0
 export CUDA_LAUNCH_BLOCKING=0
 
 ./tensorflow-agent predict urls --model_name=$MODELNAME --duplicate_input=$DUPLICATE_INPUT --database_address=$DATABASE_ADDRESS --publish --use_gpu --disable_autotune=true --batch_size=$BATCHSIZE \
-  --trace_level=MODEL_TRACE --database_name=$DATABASE_NAME
+  --trace_level=MODEL_TRACE --database_name=$DATABASE_NAME  --gpu_device_id=$GPU_DEVICE_ID
 
 export CUDA_LAUNCH_BLOCKING=1
 
 # run framework trace to get acurate layer latency
 ./tensorflow-agent predict urls --model_name=$MODELNAME --duplicate_input=$DUPLICATE_INPUT --batch_size=$BATCHSIZE --database_address=$DATABASE_ADDRESS --publish --use_gpu --disable_autotune=true \
-  --trace_level=FRAMEWORK_TRACE --database_name=$DATABASE_NAME
+  --trace_level=FRAMEWORK_TRACE --database_name=$DATABASE_NAME  --gpu_device_id=$GPU_DEVICE_ID
 
 export CUDA_LAUNCH_BLOCKING=0
 
 # run system library trace to get acurate gpu kernel latency
 ./tensorflow-agent predict urls --model_name=$MODELNAME --duplicate_input=$DUPLICATE_INPUT --batch_size=$BATCHSIZE --database_address=$DATABASE_ADDRESS --publish --use_gpu --disable_autotune=true \
-  --trace_level=SYSTEM_LIBRARY_TRACE --database_name=$DATABASE_NAME
+  --trace_level=SYSTEM_LIBRARY_TRACE --database_name=$DATABASE_NAME --gpu_device_id=$GPU_DEVICE_ID
 
 # run system library trace with gpu metrics to get metrics of each cuda kernel
 ./tensorflow-agent predict urls --model_name=$MODELNAME --duplicate_input=$DUPLICATE_INPUT --batch_size=$BATCHSIZE --database_address=$DATABASE_ADDRESS --publish --use_gpu --disable_autotune=true \
-  --trace_level=SYSTEM_LIBRARY_TRACE --gpu_metrics=flop_count_sp --database_name=$DATABASE_NAME
+  --trace_level=SYSTEM_LIBRARY_TRACE --gpu_metrics=flop_count_sp --database_name=$DATABASE_NAME --gpu_device_id=$GPU_DEVICE_ID
 
 ./tensorflow-agent predict urls --model_name=$MODELNAME --duplicate_input=$DUPLICATE_INPUT --batch_size=$BATCHSIZE --database_address=$DATABASE_ADDRESS --publish --use_gpu --disable_autotune=true \
-  --trace_level=SYSTEM_LIBRARY_TRACE --gpu_metrics=dram_read_bytes,dram_write_bytes --database_name=$DATABASE_NAME
+  --trace_level=SYSTEM_LIBRARY_TRACE --gpu_metrics=dram_read_bytes,dram_write_bytes --database_name=$DATABASE_NAME --gpu_device_id=$GPU_DEVICE_ID
+
