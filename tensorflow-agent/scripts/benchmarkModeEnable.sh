e4497b9ccd021fffd1cad0d724da1a9e7465fa1a
Cheng Li <cli99@illinois.edu>
2019-07-29 16:36:26 -0700

    Update benchmarkModeEnable.sh

--- tensorflow-agent/scripts/benchmarkModeEnable.sh
+++ tensorflow-agent/scripts/benchmarkModeEnable.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/optimize_gpu.html
 for GPU in $(nvidia-smi --format=csv,noheader --query-gpu=uuid); do
   sudo nvidia-smi -rac -i $GPU
   MEMclock=$(nvidia-smi -q -i 0 -d CLOCK | grep -A2 Default\ Applications\ Clocks | grep Memory | tr -d \  | cut -d: -f2 | sed s/MHz//g | sort -nr | head -n1)
