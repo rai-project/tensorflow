faa16a417c4a79fdd495ef1cb0ea3c2913de78ef
Abdul Dakkak <dakkak@illinois.edu>
2019-11-01 19:39:34 +0000

    work

--- tensorflow-agent/scripts/eurosys_cpu.sh
+++ tensorflow-agent/scripts/eurosys_cpu.sh
@@ -4,8 +4,8 @@ DATABASE_ADDRESS=$1
 BATCHSIZE=$2
 MODELNAME=$3
 NUMPREDS=200
-OUTPUTFOLDER=output_gpu
-DATABASE_NAME=eurosys_gpu
+OUTPUTFOLDER=output_cpu
+DATABASE_NAME=eurosys_cpu
 GPU_DEVICE_ID=0
 
 cd ..
@@ -26,6 +26,6 @@ export CUDA_LAUNCH_BLOCKING=0
 
 for ((b = 1; b <= $BATCHSIZE; b *= 2)); do
   echo MODEL_TRACE with batch size $BATCHSIZE
-  ./tensorflow-agent predict urls --model_name=$MODELNAME --duplicate_input=$(($NUMPREDS * $b)) --database_address=$DATABASE_ADDRESS --publish --use_gpu --disable_autotune=true --batch_size=$b \
+  ./tensorflow-agent predict urls --model_name=$MODELNAME --duplicate_input=$(($NUMPREDS * $b)) --database_address=$DATABASE_ADDRESS --publish --batch_size=$b \
     --trace_level=MODEL_TRACE --database_name=$DATABASE_NAME --gpu_device_id=$GPU_DEVICE_ID
 done
--- tensorflow-agent/scripts/gpu_all.sh
+++ tensorflow-agent/scripts/gpu_all.sh
@@ -41,7 +41,7 @@ declare -a array3=(
 #   echo $i
 #   ./gpu_eval_ab.sh localhost 2 $i
 #   for ((b = 2; b <= 2; b *= 2)); do
-#     ./gpu_eval_fb.sh localhost $b $i
+#     ./eurosys_gpu.sh localhost $b $i
 #     ./gpu_analysis.sh localhost $b $i
 #   done
 # done
@@ -50,7 +50,7 @@ declare -a array3=(
 #   echo $i
 #   ./gpu_eval_ab.sh localhost 16 $i
 #   for ((b = 2; b <= 16; b *= 2)); do
-#     ./gpu_eval_fb.sh localhost $b $i
+#     ./eurosys_gpu.sh localhost $b $i
 #     ./gpu_analysis.sh localhost $b $i
 #   done
 # done
@@ -59,31 +59,22 @@ declare -a array3=(
 #   echo $i
 #   ./gpu_eval_ab.sh localhost 256 $i
 #   for ((b = 2; b <= 256; b *= 2)); do
-#     ./gpu_eval_fb.sh localhost $b $i
+#     ./eurosys_gpu.sh localhost $b $i
 #     ./gpu_analysis.sh localhost $b $i
 #   done
 # done
 
 for i in "${array1[@]}"; do
   echo $i
-  ./gpu_eval_ab.sh localhost 2 $i
-  for ((b = 1; b <= 1; b *= 2)); do
-    ./gpu_eval_fb.sh localhost $b $i
-  done
+  ./eurosys_gpu.sh localhost 1 $i
 done
 
 for i in "${array2[@]}"; do
   echo $i
-  ./gpu_eval_ab.sh localhost 2 $i
-  for ((b = 1; b <= 1; b *= 2)); do
-    ./gpu_eval_fb.sh localhost $b $i
-  done
+  ./eurosys_gpu.sh localhost 1 $i
 done
 
 for i in "${array3[@]}"; do
   echo $i
-  ./gpu_eval_ab.sh localhost 2 $i
-  for ((b = 1; b <= 1; b *= 2)); do
-    ./gpu_eval_fb.sh localhost $b $i
-  done
+  ./eurosys_gpu.sh localhost 1 $i
 done
